84bcfa040c3d5357d84a91a0cd7168fee76864df
catscarlet <i@catscarlet.com>
2015-07-17 11:28:05 +0800

    更新

--- gb2312toutf8.sh
+++ gb2312toutf8.sh
@@ -4,15 +4,10 @@ OUTPUT=utf8
 for i in `find . -name '*.php'` ;
 
 do
-
 #echo "Found file "$i "!!!";\
 newpath=`php main.php $i`
 #echo "The new path is "$OUTPUT/$newpath
-
 mkdir -p $OUTPUT/$newpath
-
 iconv -c -f gb2312 -t utf8 $i -o /tmp/iconv.tmp;
-
 mv -fv /tmp/iconv.tmp ./$OUTPUT/$i;
-
 done
--- main.php
+++ main.php
@@ -1,10 +1,6 @@
 <?php
-
-
 $s=$argv[1];
-
 $l=strrpos($s,'/');
 $r=substr($s,0,$l);
 echo $r;
-
- ?>
+?>
